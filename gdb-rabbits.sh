538765fa5ebf9385f59a62f37128635997213c6d
Frédéric Pétrot <frederic.petrot@univ-grenoble-alpes.fr>
2017-11-03 16:36:16 +0100

    Just making nucleo the defaut target to be debugged

--- gdb-rabbits.sh
+++ gdb-rabbits.sh
@@ -17,6 +17,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-RABBITS_EXEC_PREFIX="gdb --args" ./rabbits.sh "$@"
+RABBITS_EXEC_PREFIX="gdb --args" ./nucleo-f401re.sh "$@"
 
 # vim: ts=4 sw=4 sts=0 expandtab
